70dede11e1356b43f582b7ae9000ae2d773b683b
gingaminga <gingahae@gmail.com>
2023-01-17 00:42:13 +0900

    Feat: 개발환경 구축
    gulp로 exprees + nodemon 환경 구축

---
+++ gulpfile.ts
@@ -0,0 +1,53 @@
+import { dest, lastRun, parallel, series, src, watch } from "gulp";
+import nodemon from "gulp-nodemon";
+import ts from "gulp-typescript";
+
+const tsProject = ts.createProject("tsconfig.json");
+const tsConfig = tsProject.config;
+const tsOption = tsProject.options;
+const OUT_PATH = tsOption.outDir || "dist";
+const TRANSPILE_PATH = tsConfig.include || "./src/**/*.ts";
+
+const transpile = () =>
+  src(TRANSPILE_PATH, {
+    allowEmpty: true,
+    since: lastRun(transpile),
+    sourcemaps: true,
+  })
+    .pipe(tsProject())
+    .pipe(
+      dest(OUT_PATH, {
+        sourcemaps: ".",
+      }),
+    );
+
+const monit = () => {
+  const watcher = watch(TRANSPILE_PATH, transpile);
+
+  watcher.on("change", (path) => {
+    // 파일 내용 변경 이벤트
+    console.log(`File ${path} was changed`);
+  });
+
+  watcher.on("add", (path) => {
+    // 파일 추가 이벤트
+    console.log(`File ${path} was added`);
+  });
+
+  watcher.on("unlink", (path) => {
+    // 파일 삭제 이벤트
+    console.log(`File ${path} was removed`);
+  });
+};
+
+const start = () =>
+  nodemon({
+    delay: 500,
+    // ext: "ts",
+    script: OUT_PATH,
+    // watch: ["src"], // Gulp watch를 사용하기 때문에 불필요
+  });
+
+export const build = series(transpile);
+
+export default series(transpile, parallel(monit, start));
