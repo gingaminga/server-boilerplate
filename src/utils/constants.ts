b5025dab6ddb7e474649586ea32018a79cd9c662
gingaminga <gingahae@gmail.com>
2023-01-18 17:57:56 +0900

    Chore: 상수값 추가

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
+  NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 3001, // 서버 포트
+  PROJECT_NAME: "LIP",
 };
