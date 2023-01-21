97baba81bc63539082db3f125409e645e2b2d6c5
gingaminga <gingahae@gmail.com>
2023-01-21 23:44:20 +0900

    Style: 파일명 변경

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import requestInfoMiddleware from "@middlewares/requestInfoMiddleware";
+import requestInfo from "@middlewares/requestInfo";
 import constants from "@utils/constants";
 import express from "express";
 
 const app = express();
 app.set("port", constants.PORT);
 
-app.use(requestInfoMiddleware);
+app.use(requestInfo);
 
 export default app;
