ab7cec9b960098f456351d6ec6a58ecb1cb2da56
gingaminga <gingahae@gmail.com>
2023-01-23 16:46:20 +0900

    Feat: 프로젝트명 env로 설정

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,5 +4,5 @@ export default {
   LOG_PATH: process.env.LOG_PATH,
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 3001, // 서버 포트
-  PROJECT_NAME: "LIP",
+  PROJECT_NAME: process.env.PROJECT_NAME || "Sample Project Name",
 };
