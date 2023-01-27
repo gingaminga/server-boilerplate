ec1adfa053888cf9b8247f5846c1ab0c7b643c47
gingaminga <gingahae@gmail.com>
2023-01-27 11:35:50 +0900

    🏷️ Fix: 타입 지정
    env 값은 string이기 때문에 number type의 경우 강제 형변환 진행

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 export default {
-  LOG_MAX_FILES: process.env.LOG_MAX_FILES || 3,
-  LOG_MAX_SIZE: process.env.LOG_MAX_SIZE || 10,
+  LOG_MAX_FILES: Number(process.env.LOG_MAX_FILES || 3),
+  LOG_MAX_SIZE: Number(process.env.LOG_MAX_SIZE || 10),
   LOG_PATH: process.env.LOG_PATH,
   NODE_ENV: process.env.NODE_ENV || "development",
-  PORT: process.env.PORT || 3001, // 서버 포트
+  PORT: Number(process.env.PORT || 3001), // 서버 포트
   PROJECT_NAME: process.env.PROJECT_NAME || "Sample Project Name",
 };
