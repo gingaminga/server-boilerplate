e899c55cefd9b43a9555bcbb978f43298164a9b3
gingaminga <gingahae@gmail.com>
2023-02-01 00:53:53 +0900

    👔 Fix: 상태 API 컨트롤러 상세화
    상태를 체크하여 응답값 다르게 전달하기

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,11 +1,22 @@
+import StatusService from "@services/status";
 import { RequestHandler, Router } from "express";
+import Container from "typedi";
 
 const router = Router();
 
-const checkStatus: RequestHandler = (req, res) => {
-  res.send("OK");
+export const checkStatusController: RequestHandler = (req, res) => {
+  const statusService = Container.get(StatusService);
+  const isGood = statusService.getServerStatus();
+
+  if (isGood) {
+    res.result("OK :)");
+
+    return;
+  }
+
+  res.result("Not OK :(");
 };
 
-router.get("/status", checkStatus);
+router.get("/status", checkStatusController);
 
 export default router;
