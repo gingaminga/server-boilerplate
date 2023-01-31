dd24d2e0c0b4cdfdfb2f47eb5edc3cf32994b51f
gingaminga <gingahae@gmail.com>
2023-02-01 00:12:07 +0900

    ⚰️ Fix: sample 예시 제거

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,6 @@
-import sample from "@routers/sample";
 import { RequestHandler, Router } from "express";
 
 const router = Router();
-
-// 라우터 미들웨어
-router.use("/sample", sample);
 
 const checkStatus: RequestHandler = (req, res) => {
   res.send("OK");
