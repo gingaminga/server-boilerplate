193440fb271e974ade68cd44c8d9b89c31ab643a
gingaminga <gingahae@gmail.com>
2023-02-03 15:47:53 +0900

    ✅ Test: 테스트 코드 작성
    test에 필요한 loader 정의
    unit test를 하도록 수정

---
+++ src/test/unit/services/status.test.ts
@@ -0,0 +1,18 @@
+import StatusService from "@services/status";
+import { Container } from "typedi";
+
+describe("서버의 상태를 저장하는 서비스 :)", () => {
+  const statusService = Container.get(StatusService);
+
+  test("서버 상태 확인하기", () => {
+    const serverStatus = statusService.getServerStatus();
+    expect(serverStatus).toEqual(false);
+  });
+
+  test("서버 상태 변경하기", () => {
+    statusService.setServerStatus(true);
+
+    const serverStatus = statusService.getServerStatus();
+    expect(serverStatus).toEqual(true);
+  });
+});
