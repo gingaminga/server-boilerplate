7c73d4583e8822110c7893699e39c4f503cc55fb
gingaminga <gingahae@gmail.com>
2023-02-07 00:44:10 +0900

    🚚 Rename: middleware 명시

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import notFound from "@middlewares/not-found.middleware";
-import requestInfo from "@middlewares/request-info.middleware";
-import responseFormat from "@middlewares/response-format.middleware";
+import notFoundMiddleware from "@middlewares/not-found.middleware";
+import requestInfoMiddleware from "@middlewares/request-info.middleware";
+import responseFormatMiddleware from "@middlewares/response-format.middleware";
 import routers from "@routes/status.route";
 import constants from "@utils/constants";
 import express from "express";
@@ -10,10 +10,10 @@ app.set("port", constants.PORT);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(requestInfo);
-app.use(responseFormat);
+app.use(requestInfoMiddleware);
+app.use(responseFormatMiddleware);
 
 app.use("/api", routers);
-app.use(notFound);
+app.use(notFoundMiddleware);
 
 export default app;
