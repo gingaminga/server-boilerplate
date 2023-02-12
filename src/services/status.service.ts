ba5c76b74411ad2e09bd71578c01f2ba162a6fb1
gingaminga <gingahae@gmail.com>
2023-02-13 00:44:46 +0900

    🚩 Feat: Redis 사용하도록 기능 구현
    서버 상태를 Redis에 저장하고 가져오는 로직을 추가함

--- src/services/status.service.ts
+++ src/services/status.service.ts
@@ -1,14 +1,41 @@
-import { Service } from "typedi";
+import RedisClient from "@databases/redis/client";
+import redisKey from "@databases/redis/key";
+import logger from "@utils/logger";
+import { Inject, Service } from "typedi";
 
 @Service()
 export default class StatusService {
-  private serverStatus = false; // 서버 상태
-
-  getServerStatus() {
-    return this.serverStatus;
+  constructor(@Inject() private redisClient: RedisClient) {
+    /* empty */
   }
 
-  setServerStatus(status: boolean) {
-    this.serverStatus = status;
+  async getServerStatus() {
+    try {
+      let status = await this.redisClient.get<boolean>(redisKey.SERVER_STATUS);
+
+      if (!status) {
+        // 상태가 없을 경우 설정
+        await this.setServerStatus(true);
+        status = await this.redisClient.get<boolean>(redisKey.SERVER_STATUS);
+      }
+
+      return status;
+    } catch (error) {
+      logger.error(error);
+
+      return false;
+    }
+  }
+
+  async setServerStatus(status: boolean) {
+    try {
+      await this.redisClient.set(redisKey.SERVER_STATUS, status);
+
+      return true;
+    } catch (error) {
+      logger.error(error);
+
+      return false;
+    }
   }
 }
