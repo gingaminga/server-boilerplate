48396302a936df4626890cc5749aebf3e7fe4f76
gingaminga <gingahae@gmail.com>
2023-02-19 22:35:37 +0900

    ✨ Feat: 유틸 함수 추가
    JSON parsing 함수 추가

---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @description JSON parsing하기
+ * @param value JSON.stringify가 적용된 문자열
+ */
+export const parseJSON = <T>(value: string) => {
+  try {
+    const realValue: T = JSON.parse(value);
+
+    return realValue;
+  } catch (error) {
+    /* empty */
+  }
+
+  return value as T;
+};
