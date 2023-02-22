02b7e80430ab223ea6c2d55628671b3019f2a056
gingaminga <gingahae@gmail.com>
2023-02-22 15:11:47 +0900

    🔥 Remove: 불필요한 try/catch 제거
    에러 핸들링 처리를 했기 때문에, 불필요한 로직 제거

--- src/controllers/status.controller.ts
+++ src/controllers/status.controller.ts
@@ -3,21 +3,17 @@ import { RESPONSE_MESSAGE } from "@utils/response";
 import { RequestHandler } from "express";
 
 export const checkStatusController: RequestHandler = async (req, res) => {
-  try {
-    const isHtml = Boolean(req.query.html);
+  const isHtml = Boolean(req.query.html);
 
-    const isGood = await statusService.getServerStatus();
+  const isGood = await statusService.getServerStatus();
 
-    const data = isGood ? RESPONSE_MESSAGE.GOOD : RESPONSE_MESSAGE.BAD;
+  const data = isGood ? RESPONSE_MESSAGE.GOOD : RESPONSE_MESSAGE.BAD;
 
-    if (isHtml) {
-      res.send(data);
+  if (isHtml) {
+    res.send(data);
 
-      return;
-    }
-
-    res.result(data);
-  } catch (error) {
-    res.error(error);
+    return;
   }
+
+  res.result(data);
 };
