6b949393a13ed12763d81e806299182e0101989a
gingaminga <gingahae@gmail.com>
2023-02-28 23:53:51 +0900

    🔥 Remove: route 관련 부분 삭제 및 영향있는 파일 수정

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
-import errorHandlerMiddleware from "@middlewares/error-handler.middleware";
 import notFoundMiddleware from "@middlewares/not-found.middleware";
 import requestInfoMiddleware from "@middlewares/request-info.middleware";
 import responseFormatMiddleware from "@middlewares/response-format.middleware";
-import routers from "@routes/status.route";
 import express from "express";
 
 const app = express();
@@ -11,9 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(requestInfoMiddleware);
 app.use(responseFormatMiddleware);
-
-app.use("/api", routers);
 app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
 export default app;
