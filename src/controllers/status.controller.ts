ee67daf1483604c42f2f03c8349901213f9cf41e
gingaminga <gingahae@gmail.com>
2023-03-01 14:18:29 +0900

    ✨ Feat: 전달받은 상태 확인
    전달받은 상태가 맞는지 확인하는 컨트롤러 작성

--- src/controllers/status.controller.ts
+++ src/controllers/status.controller.ts
@@ -1,5 +1,15 @@
+import { TStatusEventData } from "@customTypes/socket";
+import { statusService } from "@loaders/service.loader";
 import { Socket } from "socket.io";
 
-export default (socket: Socket, message: any) => {
-  socket.emit("test", 10);
+export default (socket: Socket, data: TStatusEventData) => {
+  const isGood = data === "good";
+  const status = statusService.getServerStatus();
+
+  const message = status === isGood ? "correct" : "incorrect";
+  const response = {
+    data: message,
+  };
+
+  socket.emit("status", response);
 };
