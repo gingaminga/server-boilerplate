117defce841ec304c4b70ca0cc9b174c0bcceb11
gingaminga <gingahae@gmail.com>
2023-03-05 14:26:19 +0900

    ✅ Test: 테스트 로직 추가

---
+++ src/test/unit/controllers/status.controller.test.ts
@@ -0,0 +1,78 @@
+import statusController from "@controllers/status.controller";
+import { TStatusEventData } from "@customTypes/socket";
+import { statusService } from "@loaders/service.loader";
+import { Socket } from "socket.io";
+
+const socket = {
+  emit: jest.fn(),
+} as unknown as Socket;
+
+describe("Status controller test :)", () => {
+  let isGood: TStatusEventData;
+
+  beforeEach(() => {
+    isGood = "good";
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("Server status is bad", () => {
+    beforeAll(() => {
+      jest.spyOn(statusService, "getServerStatus").mockReturnValue(false);
+    });
+
+    test("Should emit incorrect with parameter is good", () => {
+      statusController(socket, isGood);
+
+      expect(socket.emit).toBeCalledWith(
+        "status",
+        expect.objectContaining({
+          data: "incorrect",
+        }),
+      );
+    });
+
+    test("Should emit correct with parameter is bad", () => {
+      isGood = "bad";
+      statusController(socket, isGood);
+
+      expect(socket.emit).toBeCalledWith(
+        "status",
+        expect.objectContaining({
+          data: "correct",
+        }),
+      );
+    });
+  });
+
+  describe("Server status is good", () => {
+    beforeAll(() => {
+      jest.spyOn(statusService, "getServerStatus").mockReturnValue(true);
+    });
+
+    test("Should emit correct with parameter is good", () => {
+      statusController(socket, isGood);
+
+      expect(socket.emit).toBeCalledWith(
+        "status",
+        expect.objectContaining({
+          data: "correct",
+        }),
+      );
+    });
+
+    test("Should emit incorrect with parameter is bad", () => {
+      isGood = "bad";
+      statusController(socket, isGood);
+
+      expect(socket.emit).toBeCalledWith(
+        "status",
+        expect.objectContaining({
+          data: "incorrect",
+        }),
+      );
+    });
+  });
+});
---
+++ src/test/unit/middlewares/validator.socket.middleware.test.ts
@@ -0,0 +1,52 @@
+import validatorSocketMiddleware from "@middlewares/validator.socket.middleware";
+import { Event, Socket } from "socket.io";
+
+const socket = {} as Socket;
+const next = jest.fn();
+
+describe("Validator socket middleware test :)", () => {
+  let packet: Event;
+
+  beforeEach(() => {
+    packet = ["", {}];
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test(`Should thorw error when data format is wrong`, async () => {
+    await validatorSocketMiddleware(socket, packet, next);
+    expect(next).toBeCalledTimes(1);
+  });
+
+  test(`Should thorw error when data value is wrong`, async () => {
+    packet[0] = "status";
+    packet[1] = {
+      data: "",
+    };
+
+    await validatorSocketMiddleware(socket, packet, next);
+    expect(next).toBeCalledTimes(1);
+  });
+
+  test(`Should success when eventName is status and data is good`, async () => {
+    packet[0] = "status";
+    packet[1] = {
+      data: "good",
+    };
+
+    await validatorSocketMiddleware(socket, packet, next);
+    expect(next).toBeCalledTimes(1);
+  });
+
+  test(`Should success when eventName is status and data is bad`, async () => {
+    packet[0] = "status";
+    packet[1] = {
+      data: "good",
+    };
+
+    await validatorSocketMiddleware(socket, packet, next);
+    expect(next).toBeCalledTimes(1);
+  });
+});
