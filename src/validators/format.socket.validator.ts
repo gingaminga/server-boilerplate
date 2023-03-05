fc812839308f3ee4624d52a7675387281ef64ef8
gingaminga <gingahae@gmail.com>
2023-03-05 14:24:29 +0900

    🚑 Fix: data format
    빈 객체를 검증할 때는 error가 발생하지 않아, 필수로 있어야하는 key에 require를 지정

--- src/validators/format.socket.validator.ts
+++ src/validators/format.socket.validator.ts
@@ -2,7 +2,7 @@ import { ISocketDataFormat } from "@customTypes/socket";
 import { Joi } from "celebrate";
 
 export const checkCommonSocketDataSchema = Joi.object<ISocketDataFormat<unknown>>().keys({
-  data: Joi.any(),
+  data: Joi.any().required(),
 });
 
 export const checkStatusEventDataSchema = Joi.string().valid("good", "bad");
